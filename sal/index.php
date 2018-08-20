e580a63f78fe5d6478d06dca9a3e109eccb56d7b
madbasanta <42416735+madbasanta@users.noreply.github.com>
2018-08-20 22:48:58 +0545

    Update index.php

--- sal/index.php
+++ sal/index.php
@@ -4,5 +4,5 @@ session_start();
 require 'core/bootstrap.php';
 
 require 'functions.php';
-dd("index");
-//require "controllers/".Router::load('routes.php')->direct(Request::uri(), Request::method());
+
+require "controllers/".Router::load('routes.php')->direct(Request::uri(), Request::method());
