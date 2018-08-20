1fb3c6758fb4e6429c2a2e701be8dc9e31d3558e
callmebasanta44 <callmebasanta44@gmail.com>
2018-08-20 23:12:38 +0545

    config.php update

--- sal/controllers/index.php
+++ sal/controllers/index.php
@@ -1,18 +1,23 @@
 <?php
 
 $visitors = $app["database"]->all('visitors');
-if(is_array($visitors) && count($visitors))
-	$visitors = $visitors[0];
-else
-	$visitors = 0;
 
-if($visitors)
+if(is_array($visitors) && count($visitors)){
+	$visitors = $visitors[0];
+}
+else{
+	$visitors = 0;
+}
+
+if($visitors){
 	if(!authCheck())
 		$app['database']->update('visitors', $visitors->id, [
 			"visitors" => $visitors->visitors + 1
 		]);
-else
-	$app["database"]->insert('visitors', ['visitors' => 1]);
+}
+else{
+	$result = $app["database"]->insert('visitors', ['visitors' => 1]);
+}
 
 $visitors = $app["database"]->all('visitors')[0];
 $count = $visitors->visitors;
