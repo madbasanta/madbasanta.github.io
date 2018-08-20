2e4ff743b9d134c9a7a31250ba495d934cd05663
madbasanta <42416735+madbasanta@users.noreply.github.com>
2018-08-20 22:46:34 +0545

    Update index.php

--- sal/controllers/index.php
+++ sal/controllers/index.php
@@ -1,21 +1,21 @@
 <?php
 
-//$visitors = $app["database"]->all('visitors');
-//if(is_array($visitors) && count($visitors))
-//	$visitors = $visitors[0];
-//else
-	//$visitors = 0;
+$visitors = $app["database"]->all('visitors');
+if(is_array($visitors) && count($visitors))
+	$visitors = $visitors[0];
+else
+	$visitors = 0;
 
-//if($visitors)
-//	if(!authCheck())
-//		$app['database']->update('visitors', $visitors->id, [
-//			"visitors" => $visitors->visitors + 1
-//		]);
-//else
-//	$app["database"]->insert('visitors', ['visitors' => 1]);
+if($visitors)
+	if(!authCheck())
+		$app['database']->update('visitors', $visitors->id, [
+			"visitors" => $visitors->visitors + 1
+		]);
+else
+	$app["database"]->insert('visitors', ['visitors' => 1]);
 
-//$visitors = $app["database"]->all('visitors')[0];
-// $count = $visitors->visitors;
-//$count = 2;
+$visitors = $app["database"]->all('visitors')[0];
+$count = $visitors->visitors;
+$count = 2;
 echo "this is test";
-//require 'views/index.view.php';
+require 'views/index.view.php';
